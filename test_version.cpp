2334a375a3a5be4681c76bf62c5bd284b2325ace
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-03-29 13:57:04 +0300

    add boost test

---
+++ test_version.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE test_version
+
+#include <boost/test/unit_test.hpp>
+
+#include "lib.h"
+
+BOOST_AUTO_TEST_SUITE(test_version)
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+    BOOST_CHECK(version() > 0);
+}
+
+}
